a79451fa0c70304592edffa1254324a530b19889
MMaithreyan <maithreyanm8@gmail.com>
2022-05-27 00:37:21 +0530

    code added

--- script.js
+++ script.js
@@ -3,32 +3,32 @@ let nxt = document.querySelectorAll(".nxt_btn1");
 let container = document.querySelectorAll(".container1");
 
 let prv2 = document.querySelectorAll(".prv_btn2");
-let nxt2 = document.querySelectorAll(".prv_btn2");
+let nxt2 = document.querySelectorAll(".nxt_btn2");
 let cont2 = document.querySelectorAll(".container2");
 
-let prv3 = document.querySelectorAll("prv_btn3");
-let nxt3 = document.querySelectorAll("nxt_btn3");
-let cont3 = document.querySelectorAll("container3");
+let prv3 = document.querySelectorAll(".prv_btn3");
+let nxt3 = document.querySelectorAll(".nxt_btn3");
+let cont3 = document.querySelectorAll(".container3");
 
-let prv4 = document.querySelectorAll("prv_btn4");
-let nxt4 = document.querySelectorAll("nxt_btn4");
-let cont4 = document.querySelectorAll("container4");
+let prv4 = document.querySelectorAll(".prv_btn4");
+let nxt4 = document.querySelectorAll(".nxt_btn4");
+let cont4 = document.querySelectorAll(".container4");
 
-let prv5 = document.querySelectorAll("prv_btn5");
-let nxt5 = document.querySelectorAll("nxt_btn5");
-let cont5 = document.querySelectorAll("container5");
+let prv5 = document.querySelectorAll(".prv_btn5");
+let nxt5 = document.querySelectorAll(".nxt_btn5");
+let cont5 = document.querySelectorAll(".container5");
 
-let prv6 = document.querySelectorAll("prv_btn6");
-let nxt6 = document.querySelectorAll("nxt_btn6");
-let cont6 = document.querySelectorAll("container6");
+let prv6 = document.querySelectorAll(".prv_btn6");
+let nxt6 = document.querySelectorAll(".nxt_btn6");
+let cont6 = document.querySelectorAll(".container6");
 
-let prv7 = document.querySelectorAll("prv_btn7");
-let nxt7 = document.querySelectorAll("nxt_btn7");
-let cont7 = document.querySelectorAll("container7");
+let prv7 = document.querySelectorAll(".prv_btn7");
+let nxt7 = document.querySelectorAll(".nxt_btn7");
+let cont7 = document.querySelectorAll(".container7");
 
-let prv8 = document.querySelectorAll("prv_btn8");
-let nxt8 = document.querySelectorAll("nxt_btn8");
-let cont8 = document.querySelectorAll("container8");
+let prv8 = document.querySelectorAll(".prv_btn8");
+let nxt8 = document.querySelectorAll(".nxt_btn8");
+let cont8 = document.querySelectorAll(".container8");
 
 
 prv.forEach((left)=>{
