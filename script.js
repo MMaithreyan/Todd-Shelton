6f53e864c6d456fd00fd39c7eeb3ccf2cf140f66
MMaithreyan <maithreyanm8@gmail.com>
2022-05-26 21:58:45 +0530

    code added

--- script.js
+++ script.js
@@ -2,23 +2,38 @@ let prv = document.querySelectorAll(".prv_btn");
 let nxt = document.querySelectorAll(".nxt_btn");
 let container = document.querySelectorAll(".container");
 
-for(i=0 ; i<prv.length ; i++){
- prv[i].addEventListener("click", function () {
-     console.log("hello");
-     document.querySelector(".container").scrollLeft -= 450;
- });
-}
-
-for(j=0 ; j<nxt.length ; j++){
- nxt[j].addEventListener("click", function () {
-     console.log("hello");
-     document.querySelector(".container").scrollLeft += 450;
- });
-}
+ let i,j;
+// for(i=0 ; i<prv.length ; i++){
+//  prv[i].addEventListener("click", function () {
+//      console.log("hello");
+//      container.scrollLeft -= 450;
+//  });
+// }
 
 
+// for(j=0 ; j<nxt.length ; j++){
+//  nxt[j].addEventListener("click", function () {
+//      console.log("hello");
+//      container.scrollLeft += 450;
+//  });
+// }
 
 
+prv.forEach((left)=>{
+    left.addEventListener("click",()=>{
+        container.forEach((move)=>{
+            move.scrollLeft -= 450;
+        })
+    })
+})
+
+nxt.forEach((right)=>{
+    right.addEventListener("click",()=>{
+        container.forEach((move)=>{
+            move.scrollLeft += 450;
+        })
+    })
+})
 
 // let i, j;
 // for (i = 0; i < prv.length; i++) {
